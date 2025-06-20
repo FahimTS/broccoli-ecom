03d351eceaf22b17fdca80eacb12f8b40da557ee
Md Fahim <mdfahimhossen629@gmail.com>
2025-06-20 11:41:49 +0600

    cart page some design

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,41 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit{
-  menuIconXbar:boolean = true;
+export class NavbarComponent implements OnInit {
+  menuIconXbar: boolean = true;
   menuBars: boolean = false;
-  openMenu(){
-    this.menuBars =! this.menuBars;
-    this.menuIconXbar =! this.menuIconXbar;
-  }
 
-  isCartOpen:boolean = false;
-toggleCart() {
-  this.isCartOpen = !this.isCartOpen;
-}
-
- cartItems: any[] = [];
+  isCartOpen: boolean = false;
+  cartItems: any[] = [];
   subtotal: number = 0;
-totalQuantity: number = 0;
+  totalQuantity: number = 0;
 
-  constructor(private cartService: CartService) {}
+  currentRoute: string = '';
+
+  constructor(private cartService: CartService, private router: Router) {
+    // detect route change
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.urlAfterRedirects;
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.cartService.cart$.subscribe((items) => {
       this.cartItems = items;
       this.subtotal = this.cartService.getSubtotal();
       this.totalQuantity = this.cartService.getTotalQuantity();
-
     });
 
     this.cartService.cartVisibility$.subscribe((visible) => {
       this.isCartOpen = visible;
     });
+  }
+
+  openMenu() {
+    this.menuBars = !this.menuBars;
+    this.menuIconXbar = !this.menuIconXbar;
+  }
+
+  toggleCart() {
+    this.isCartOpen = !this.isCartOpen;
   }
 
   removeFromCart(item: any) {
